4164047d5d1865697f28725aba85c25d414bad54
李文乾 <lwq202@outlook.com>
2019-12-02 16:40:50 +0800

    提交

--- NetConfigLearn/SpeedTestByCore/Program.cs
+++ NetConfigLearn/SpeedTestByCore/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace SpeedTestByCore
 {
@@ -6,7 +8,38 @@ namespace SpeedTestByCore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Stopwatch watch = new Stopwatch();
+            Console.WriteLine("开始测试10000个数组冒泡排序!");
+            for (int i = 0; i < 10; i++)
+            {
+                watch.Restart();
+                maopao();
+                watch.Stop();
+                Console.WriteLine($"{watch.ElapsedMilliseconds}");
+            }
+
+            Console.ReadLine();
+        }
+
+        static void maopao()
+        {
+            List<int> numbers = new List<int>();
+            for (int i = 0; i < 10000; i++)
+            {
+                numbers.Add(new Random().Next(0, 999999));
+            }
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                for (int j = i + 1; j < numbers.Count; j++)
+                {
+                    if (numbers[i] < numbers[j])
+                    {
+                        int temp = numbers[i];
+                        numbers[i] = numbers[j];
+                        numbers[j] = temp;
+                    }
+                }
+            }
         }
     }
 }
