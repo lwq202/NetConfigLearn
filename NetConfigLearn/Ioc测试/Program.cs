5c4ccb12d5d9bc6d7f507952d69b7aa488f397d9
李文乾 <lwq202@outlook.com>
2019-12-04 14:51:01 +0800

    提交

--- NetConfigLearn/Ioc测试/Program.cs
+++ NetConfigLearn/Ioc测试/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using AOPLearn.BaseClass;
+using Autofac;
+using Autofac.Builder;
+
 
 namespace Ioc测试
 {
@@ -7,8 +11,52 @@ namespace Ioc测试
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            DI();
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// 普通的容器用法
+        /// </summary>
+        static void DI()
+        {
+            Autofac.ContainerBuilder containerBuilder = new Autofac.ContainerBuilder();
+            //单例模式
+            containerBuilder.RegisterType<OrderService>()
+                .As<IOrderService>()
+                .SingleInstance();
+
+            //一个生命周期一个实例
+            //containerBuilder.RegisterType<OrderService>()
+            //    .As<IOrderService>()
+            //    .InstancePerLifetimeScope();
+
+            //每一次被请求都是一个实例
+            //containerBuilder.RegisterType<OrderService>()
+            //    .As<IOrderService>()
+            //    .InstancePerDependency();
+
+            var container = containerBuilder.Build();
+            for (int j = 0; j < 2; j++)
+            {
+                using (var life = container.BeginLifetimeScope())
+                {
+                    for (int i = 0; i < 2; i++)
+                    {
+                        var service = life.Resolve<IOrderService>();
+                        Console.WriteLine($"{service.Id}");
+                    }
+
+
+                }
+            }
+           
+        }
+
+        static void In()
+        {
+
         }
     }
 
-    public class 
 }
